546bd3d18142815140eb816c318a3c88456fcb2a
ee49 <mithunpatellu@gmail.com>
2024-10-03 06:40:09 -0400

    FIrst langchain code

--- pycode/main.py
+++ pycode/main.py
@@ -1,7 +1,25 @@
 from langchain.llms import OpenAI
 import os
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+
 api_key = os.getenv('OPEN_API_KEY')
 llm = OpenAI(openai_api_key=api_key)
 
-result = llm("Write few romatic lines for my wife, who is 33 year old and loves to read books.")
+code_prompt = PromptTemplate(
+    template="Write a very short {language} function that will {task}",
+    input_variables=["language", "task"]
+)
+
+code_chain = LLMChain(
+    llm=llm,
+    prompt=code_prompt
+)
+
+result = code_chain({
+"language": "Python",
+    "task": "return a list of number"
+})
+
 print(result)
+
